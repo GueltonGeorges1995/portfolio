d656fb31b1f4adb83fd6f8a45c9d86c580f1d1a9
Georges <georges.guelton@gmail.com>
2020-08-17 12:20:30 +0200

    header v1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header';
+import './assets/scss/global.scss';
+
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+};
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './header.scss';
+
+
+
+const Header = () => {
+    return (
+        <header className='header-bg' id='home'>
+            <Navbar bg="light" expand="lg" fixed="top">
+                <Navbar.Brand href="#home">Georges Guelton</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                        <Nav.Link href="#services">Services</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <div className='padding-top-header-content'>
+                <h1 className='text-light text-center'>Georges Guelton</h1>
+                <h2 className='text-light text-center'>Web Developer</h2>
+            </div>
+            <div className='text-center position-header-arrow arrow-size'>
+                <a href="#about">
+                    <i className="fas fa-arrow-down text-light text-center"></i>
+                </a>
+                
+            </div>
+        </header>
+    )
+};
+export default Header;
