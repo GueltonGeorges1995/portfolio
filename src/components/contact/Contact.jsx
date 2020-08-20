f7089d0ed612c2009640823c957c4eef047c14c9
Georges <georges.guelton@gmail.com>
2020-08-20 11:49:06 +0200

    validation contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,48 +2,61 @@ import React,{useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
-
 import './contac.scss';
-
 import emailjs from 'emailjs-com';
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
-    const [inputMail, setInputMail] = useState('');
-    const [inputMessage, setInputMessage] = useState('');
-
-    const handleInputMail = (e) => {
-        setInputMail(e.target.value);
-    }
-    const handleInputMessage = (e) => {
-        setInputMessage(e.target.value);
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (inputMail.trim() !== "" && inputMessage.trim() !== "") {
-            emailjs.sendForm('gmail', 'template_LJAhOJLf', '#myForm', 'user_Oh3MDSKAXr7QKxi8tVi7C')
+    const [isMessageSent, setIsMessageSent] = useState(false);
+    
+    const { handleSubmit, register, errors } = useForm();
+    const onSubmit = (data, e ) => {
+        emailjs.sendForm('gmail', 'template_LJAhOJLf', '#myForm', 'user_Oh3MDSKAXr7QKxi8tVi7C')
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
                 console.log(error.text);
-            });
-        }
+        });
+        e.target.reset();
+        setIsMessageSent(true);
     }
+
     return (
         <div id='contact' className='py-10-rem background-section'>
             <div className='text-center'>
                 <h2 className='underline-title-section'>Contact</h2>
             </div>
             <Container className='mt-5'>
-                <Form onSubmit={handleSubmit} id='myForm'>
+                <Form onSubmit={handleSubmit(onSubmit)} id='myForm'>
                     <Form.Group controlId="formGroupEmail">
                         <Form.Label className='subtitle-contact'>Your Email Address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" onChange={handleInputMail} name='email' />
+                        <Form.Control 
+                            type="email" 
+                            placeholder="Enter email" 
+                            name='email' 
+                            ref={register({
+                                required: "Required",
+                                pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "invalid email address"
+                                }
+                             })}/>
+                        {errors.email && errors.email.message}
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlTextarea1">
                         <Form.Label className='subtitle-contact'>Your Message</Form.Label>
-                        <Form.Control as="textarea" rows="3" placeholder='Type your message' onChange={handleInputMessage} name='message' />
+                        <Form.Control 
+                            as="textarea" 
+                            rows="3" 
+                            placeholder='Type your message'  
+                            name='message' 
+                            ref={register({
+                                required: "Required",
+                            })}/>
                     </Form.Group>
-                    <Button className='btn-submit-contact' type='submit'>Send</Button>
+                    {errors.message && errors.message.message}
+                    <br/>
+                    {isMessageSent ? <p className='subtitle-contact'>Thank you for your message ! I'll be in touch as soon as possible</p> : <Button className='btn-submit-contact' type='submit'>Send</Button>}
                 </Form>
             </Container>
         </div>
